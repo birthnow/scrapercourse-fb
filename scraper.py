2aedb9254b1aefa76d51276041941778226393ce
Duncan <chienweiwhite@gmail.com>
2022-06-26 17:18:01 +0800

    6.6 批次爬取登入型網頁的資料

--- scraper.py
+++ scraper.py
@@ -1,9 +1,11 @@
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import time
 import credentials
 from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
 
 options = Options()
 options.add_argument("--disable-notifications")
@@ -28,3 +30,12 @@ for i in range(10):
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
     time.sleep(4)
 
+soup = BeautifulSoup(driver.page_source, 'lxml')
+posts = soup.find_all("div", {"class": "du4w35lb l9j0dhe7"})
+
+result = []
+for post in posts:
+    content = post.find("span", {"class": "d2edcug0 hpfvmrgz qv66sw1b c1et5uql lr9zc1uh a8c37x1j fe6kdd0r mau55g9w c8b282yb keod5gw0 nxhoafnm aigsh9s9 d3f4x2em iv3no6db jq4qci2q a3bd9o3v b1v8xokw oo9gr5id hzawbc8m"})
+    result.append(content.getText())
+
+print(result)
